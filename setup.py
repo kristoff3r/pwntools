d67376d59f5094b541817dc5bab2e18700646432
Kristoffer Søholm <k.soeholm@gmail.com>
2020-07-26 15:37:35 +0200

    Bump dependency versions

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for filename in glob.glob('pwnlib/commandline/*'):
 install_requires     = ['paramiko>=1.15.2',
                         'mako>=1.0.0',
                         'pyelftools>=0.2.4',
-                        'capstone>=3.0.5rc2', # See Gallopsled/pwntools#971, Gallopsled/pwntools#1160
+                        'capstone>=4.0.2',
                         'ropgadget>=5.3',
                         'pyserial>=2.7',
                         'requests>=2.0',
